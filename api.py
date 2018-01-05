9b7ab50980292a19f3cf473c052062aae4da5d7a
exp0nge <mdislamwork@gmail.com>
2018-01-04 22:12:51 -0500

    Add user limit

--- api.py
+++ api.py
@@ -2,8 +2,12 @@ import io
 import json
 import base64
 import logging
+from datetime import datetime
 
 from flask import Flask, jsonify, request, make_response
+from flask_redis import FlaskRedis
+from redis import StrictRedis
+
 from werkzeug.utils import secure_filename
 import cv2
 import numpy as np
@@ -12,7 +16,19 @@ import load_model
 
 logger = logging.getLogger(__name__)
 
+
+class DecodedRedis(StrictRedis):
+    @classmethod
+    def from_url(cls, url, db=None, **kwargs):
+        kwargs['decode_responses'] = True
+        return StrictRedis.from_url(url, db, **kwargs)
+
+
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  # max 5 MB
+app.config['REDIS_URL'] = 'redis://localhost:6379/0'
+REDIS_STORE = FlaskRedis.from_custom_provider(DecodedRedis, app)
+REDIS_STORE.init_app(app)
 
 ALLOWED_EXTENSIONS = ['jpeg', 'jpg']
 
@@ -27,8 +43,12 @@ def _make_error(status_code, message):
     return make_response(jsonify({'status': 'error', 'message': message}), status_code)
 
 
-def make_401(message):
+def make_400(message):
     return _make_error(401, message)
+
+
+def make_429(message):
+    return _make_error(429, message)
 
 
 def make_404(message):
@@ -41,13 +61,54 @@ def init_once():
     tfnet = load_model.pb_yolo()
 
 
+USER_METADATA_VERSION = 2
+DEFAULT_EXPIRE_SECONDS = 3600
+
+
+def make_ip_config():
+    return {
+        'now': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'counter': 1,
+        'version': USER_METADATA_VERSION,
+        'requests': 1
+    }
+
+
+@app.before_request
+def before_request():
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+
+    user_metadata = REDIS_STORE.get(ip)
+    if user_metadata is None:
+        REDIS_STORE.set(ip, json.dumps(make_ip_config()), ex=DEFAULT_EXPIRE_SECONDS)
+    else:
+        user_metadata = json.loads(user_metadata)
+        print(user_metadata)
+        if user_metadata['version'] != USER_METADATA_VERSION:
+            user_metadata = make_ip_config()
+
+        elapsed = (datetime.now() -
+                   datetime.strptime(user_metadata['now'], '%Y-%m-%d %H:%M:%S'))
+
+        user_metadata['requests'] += 1
+
+        if elapsed.total_seconds() > 20:
+            user_metadata['counter'] += 1
+            user_metadata['now'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')        
+            REDIS_STORE.set(ip, json.dumps(user_metadata), ex=DEFAULT_EXPIRE_SECONDS)
+        else:
+            REDIS_STORE.set(ip, json.dumps(user_metadata), ex=DEFAULT_EXPIRE_SECONDS)            
+            return make_429("Please wait before requesting again")
+
+
 @app.route('/detect', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return make_404('GET not permitted')
     else:
         if 'b64image' in request.form:
-            logger.info('found b64 image with len {0}'.format(len(request.form['b64image'])))
+            logger.info('found b64 image with len {0}'.format(
+                len(request.form['b64image'])))
             b64_img = request.form['b64image']
             try:
                 img_decoded = base64.b64decode(b64_img)
